2b633839410a06509abd2f104b1fa59d7964edab
alexpahom <31284624+alexpahom@users.noreply.github.com>
2023-05-23 23:29:05 +0200

    Create ex-2.3-fibb-till-100.rb

---
+++ lesson2/ex-2.3-fibb-till-100.rb
@@ -0,0 +1,10 @@
+# 3. Заполнить массив числами фибоначчи до 100
+arr = []
+first_num = 0
+second_num = 1
+loop do
+  arr.push first_num
+  first_num, second_num = second_num, first_num + second_num
+  break if first_num >= 100
+end
+puts arr
