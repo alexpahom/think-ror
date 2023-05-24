a20ed15d635354f293ad041f81949f513650fef8
alexpahom <31284624+alexpahom@users.noreply.github.com>
2023-05-24 21:48:20 +0200

    Update station.rb

--- lesson3/station.rb
+++ lesson3/station.rb
@@ -1,15 +1,8 @@
-# Класс Station (Станция):
-# Имеет название, которое указывается при ее создании
-# Может принимать поезда (по одному за раз)
-# Может возвращать список всех поездов на станции, находящиеся в текущий момент
-# Может возвращать список поездов на станции по типу (см. ниже): кол-во грузовых, пассажирских
-# Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
-
 class Station
-  attr_reader :trains, :station_name
+  attr_reader :trains, :name
 
-  def initialize(station_name)
-    @station_name = station_name
+  def initialize(name)
+    @name = name
     @trains = []
   end
 
@@ -19,13 +12,9 @@ class Station
 
   def arrival(train)
     @trains.push train
-    puts "Train ##{train.id} arrived to #{station_name} station"
-    puts "Trains on the station: #{trains.map { |t| t.id }}"
   end
 
   def departure(train)
     @trains.delete train
-    puts "Train ##{train.id} departured from #{station_name} station"
-    puts "Trains on the station: #{trains.map { |t| t.id }}"
   end
 end
