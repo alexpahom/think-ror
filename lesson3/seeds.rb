fca7f1f22fa8c8cfa74d7f3c6b6ecd02bb9bd111
alexpahom <31284624+alexpahom@users.noreply.github.com>
2023-05-24 22:00:10 +0200

    Update seeds.rb

--- lesson3/seeds.rb
+++ lesson3/seeds.rb
@@ -10,17 +10,17 @@ require_relative 'station'
 @s6 = Station.new 'California'
 @s7 = Station.new 'Washington D.C.'
 
-@r1 = Route.new(start_station: @s2, end_station: @s5)
+@r1 = Route.new(@s2, @s5)
 @r1.add_station @s1
 @r1.add_station @s4
 
-@r2 = Route.new(start_station: @s7, end_station: @s6)
+@r2 = Route.new(@s7, @s6)
 @r2.add_station @s3
 
-@t1 = Train.new(id: 1, type: :cargo, cars_number: 12)
-@t2 = Train.new(id: 2, type: :cargo, cars_number: 25)
-@t3 = Train.new(id: 3, type: :pass, cars_number: 8)
-@t4 = Train.new(id: 4, type: :pass, cars_number: 12)
+@t1 = Train.new(1, :cargo, 12)
+@t2 = Train.new(2, :cargo, 25)
+@t3 = Train.new(3, :passenger, 8)
+@t4 = Train.new(4, :passenger, 12)
 
 @t1.route = @r1
 @t2.route = @r1
