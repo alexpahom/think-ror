74d38f69590e542207cfc135c6f87e59dd14c34f
alexpahom <31284624+alexpahom@users.noreply.github.com>
2023-05-30 17:36:50 +0200

    Create route.rb

---
+++ lesson4/route.rb
@@ -0,0 +1,21 @@
+class Route
+  attr_reader :start_station, :end_station
+
+  def initialize(start_station, end_station)
+    @start_station = start_station
+    @end_station = end_station
+    @in_between_stations = []
+  end
+
+  def delete_station(station)
+    @in_between_stations.delete station
+  end
+
+  def add_station(station)
+    @in_between_stations.push station
+  end
+
+  def stations
+    [@start_station] + @in_between_stations + [@end_station]
+  end
+end
