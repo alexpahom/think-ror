81e9d7a480bf11f97d843a0608b78a53a99ed31f
alexpahom <31284624+alexpahom@users.noreply.github.com>
2023-05-30 20:09:07 +0200

    Update passenger_train.rb

--- lesson4/trains/passenger_train.rb
+++ lesson4/trains/passenger_train.rb
@@ -1,7 +1,8 @@
 require_relative 'train'
 
 class PassengerTrain < Train
-  def add_car(car)
-    super(car) if car.is_a?(PassengerCar)
+  def initialize(number)
+    @type = :passenger
+    super
   end
 end
